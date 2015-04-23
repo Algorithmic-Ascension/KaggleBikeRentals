28f5a02b1aa6f86cb8000383951c281ef0cf1176
Benjamin Chen <Algorithmic.Ascension+github@gmail.com>
2015-04-23 15:58:09 -0400

    experimenting with autoregressive model made categorical variables into strings

--- preprocess.py
+++ preprocess.py
@@ -1,23 +1,42 @@
 import csv
 import datetime as dt
 
-testSize = 168
-data = [row for row in csv.reader(open('trainRaw.csv', 'r'))]
+testSize = 24 * 7 # hours in a week
+data = [ row for row in csv.reader(open('trainRaw.csv', 'r')) ]
 
 train = data[:-testSize]
-csvwriter = csv.writer(open('train.csv', 'wb'))
+csvwriter = csv.writer( open('train.csv', 'wb') )
 # exclude the 2 columns not in test, convert datetime to date and seconds for easier learning
 # putting into dayhours since 2011 highlights that days are continuous
-for t in train:
-	timeSince2011 = (dt.datetime.strptime(t[0], "%Y-%m-%d %X") - dt.datetime(2011,1,1))
-	csvwriter.writerow([t[-1], timeSince2011.days + float(timeSince2011.seconds)/3600/24 ] + t[1:-3])
+for i in range(len(train)):
+	csvwriter.writerow( [train[i][-1]] + preprocess(train, i) )
 
-truth = train[:testSize]#[x for x in data if x not in train]
+truth = train[testSize*70:testSize*71] # fitted model for testing purposes # for realzies [x for x in data if x not in train]
 # save ground truth into similar file for easy comparison
-unlabeledWriter = csv.writer(open('test.csv', 'wb'))
-truthWriter = csv.writer(open('groundTruth.csv', 'wb'))
-for t in truth:
-	timeSince2011 = (dt.datetime.strptime(t[0], "%Y-%m-%d %X") - dt.datetime(2011,1,1))
-	unlabeledWriter.writerow([ timeSince2011.days + float(timeSince2011.seconds)/3600/24 ] + t[1:-3])
-	truthWriter.writerow( [t[-1]] )
-		
+unlabeledWriter = csv.writer( open('test.csv', 'wb') )
+truthWriter = csv.writer( open('groundTruth.csv', 'wb') )
+for i in range(len(truth)):
+	unlabeledWriter.writerow( preprocess(truth, i) )
+	truthWriter.writerow( [truth[i][-1]] )
+
+# enforces column ordering 
+def preprocess(data, rowNumber):
+	row = data[rowNumber]
+	hourlyComponent, weeklyComponent = encodeTimeAsDifferenceFromPreviousTime(data, rowNumber)
+	return [hourlyComponent, weeklyComponent] + decodeCategories(row) + row[5:-3]
+
+# Autoregressive model
+def encodeTimeAsDifferenceFromPreviousTime(data, i):	
+	hourlyComponent = float(data[i-1   ][-1]) - float(data[i][-1])
+	weeklyComponent = float(data[i-24*7][-1]) - float(data[i][-1])
+	return hourlyComponent, weeklyComponent
+	
+def decodeCategories(row):
+	seasons = ['', 'spring', 'summer', 'fall', 'winter']
+	holiday = ['noliday', 'holiday']
+	workingDay = ['leisure', 'work'] 
+	return [ int(seasons[row[1]]) , int(holiday[row[2]]) , int(workingDay[row[3]]) ]
+	
+def timeSince2011(row):
+	return (dt.datetime.strptime(t[0], "%Y-%m-%d %X") - dt.datetime(2011,1,1))
+
--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import csv
 import statistics as s
 import matplotlib.pyplot as plt
 
-results = [row for row in csv.reader(open('results.csv'    , 'r'))]
+results = [row for row in csv.reader(open(    'results.csv', 'r'))]
 truth   = [row for row in csv.reader(open('groundTruth.csv', 'r'))]
 if len(truth) != len(results):
 	print "length of truth " ,len(truth), " != length of results ", len(results) 
@@ -10,5 +10,6 @@ else:
 	n = len(results)
 	plt.plot(range(n), [float(  truth[i][0]) for i in range(n)])
 	plt.plot(range(n), [float(results[i][0]) for i in range(n)])
+	plt.ylabel('Score')
 	plt.xlabel('Time')
 	plt.show()
